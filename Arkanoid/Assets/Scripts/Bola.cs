cb0fd4b59684a906b57e3563e5b1eaeafa550601
brunomanente <bruno.manente0@gmail.com>
2019-04-26 19:26:50 -0300

    Colisao com o chao

---
+++ Arkanoid/Assets/Scripts/Bola.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour
+{
+
+    public Vector2 Velocidade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Velocidade);
+    }  
+}
---
+++ Arkanoid/Assets/Scripts/Jogador.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+
+    public float velocidade = 10.0f;
+    public float horizontalAxis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalAxis = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalAxis * velocidade, 0);
+    }
+}
